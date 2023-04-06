ccb1471c11d52cc29601696f6cb77111aa776266
Reham Abdelhameed <94580660+Rehaam10@users.noreply.github.com>
2023-04-07 01:39:28 +0200

    Update 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,15 +1,17 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * clear_bit - Entry Point
- * @n: input
- * @index: index
- * Return: 0
+ * clear_bit - sets the value of a given bit to 0
+ * @n: pointer to the number to change
+ * @index: index of the bit to clear
+ *
+ * Return: 1 for success, -1 for failure
  */
-	int clear_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 63)
 		return (-1);
 
-	*n = *n & ~(1ul << index);
+	*n = (~(1UL << index) & *n);
 	return (1);
 }
