4e453819356cba85c872ba2139358e59bc1f24be
Reham Abdelhameed <94580660+Rehaam10@users.noreply.github.com>
2023-04-07 01:43:06 +0200

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,33 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+#include "main.h"
+
 /**
- * create_file - creates a file and puts text in it
- * with 600 perms (do not change if it exists)
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
  *
- * @filename: name for file
- * @text_content: text to put into file
- *
- * Return: 1 on success, -1 on failure
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file;
-	int length = 0, inlen = 0;
-	char *ptr;
+	int fd, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (file == -1)
-		return (-1);
-
 	if (text_content != NULL)
 	{
-		for (inlen = 0, ptr = text_content; *ptr; ptr++)
-			inlen++;
-		length = write(file, text_content, inlen);
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
-	if (close(file) == -1 || inlen != length)
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
 		return (-1);
+
+	close(fd);
+
 	return (1);
 }
